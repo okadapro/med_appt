7163441decce07b8f32a05d551215760a2c53224
okadapro <okada.felipe2@gmail.com>
2024-06-14 07:45:01 -0400

    mongodb

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:<MjA5MTItb2thZGFm>@127.0.0.1:27017";
+const mongoURI =  "mongodb://root:MTg5MzUtb2thZGFm@127.0.0.1:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
