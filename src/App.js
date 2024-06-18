862f721a43b0aff59c53170df4fb7dc4c8026a72
okadapro <okada.felipe2@gmail.com>
2024-06-18 12:33:40 -0400

    commit

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Navbar from './Components/Navbar/Navbar';
 import InstantConsultation from './Components/InstantConsultation/InstantConsultation';
+import Landing_Page from './Components/Landing_Page/Landing_Page';
 
 function App() {
     return (
@@ -11,7 +12,8 @@ function App() {
             <Navbar/>
                 <Routes>
                 </Routes>
-              
+                <Route path="/" element={<Landing_Page/>}/>
+                <Route path="/instant-consultation" element={<InstantConsultation />} />
           </BrowserRouter>
          
       </div>
